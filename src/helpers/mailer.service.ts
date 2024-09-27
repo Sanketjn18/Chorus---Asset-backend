ec8ea435565961c1f869c6509aa4a4bd7066648d
thestackboy <vikrant.giri@stackmentalist.com>
2024-09-27 22:11:18 +0530

    change mail error

--- src/helpers/mailer.service.ts
+++ src/helpers/mailer.service.ts
@@ -191,9 +191,6 @@ export class MailerService {
       this.logger.error(
         'Sender email is not defined in the environment variables.',
       );
-      throw new Error(
-        'Sender email is not defined in the environment variables.',
-      );
     }
   }
 
@@ -208,7 +205,6 @@ export class MailerService {
       this.logger.log(`OTP email sent to ${to}`);
     } catch (error) {
       this.logger.error(`Failed to send email: ${error.message}`);
-      throw new Error('Failed to send email');
     }
   }
 
@@ -265,7 +261,6 @@ export class MailerService {
       this.logger.log(`Welcome email sent to ${to}`);
     } catch (error) {
       this.logger.error(`Failed to send email: ${error.message}`);
-      throw new Error('Failed to send email');
     }
   }
   generateOtp(): string {
